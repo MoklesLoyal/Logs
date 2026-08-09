d7977fd9595ae43da1807c4d59c477a976f0e9a2
MoklesLoyal <60904891+MoklesLoyal@users.noreply.github.com>
2026-08-08 20:29:24 -0400

    Add temporary info.php to discover server document root

--- info.php
+++ info.php
@@ -1,1 +1,6 @@
-<?php echo $_SERVER['DOCUMENT_ROOT']; ?>
+<?php
+header('Content-Type: text/plain');
+echo "DOCUMENT_ROOT: " . ($_SERVER['DOCUMENT_ROOT'] ?? 'N/A') . "\n";
+echo "SCRIPT_FILENAME: " . ($_SERVER['SCRIPT_FILENAME'] ?? 'N/A') . "\n";
+echo "PWD: " . getcwd() . "\n";
+?>
