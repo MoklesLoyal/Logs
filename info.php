9a331b3ff8cfb38d7ef414da184d08888ad09b03
MoklesLoyal <60904891+MoklesLoyal@users.noreply.github.com>
2026-08-08 20:31:10 -0400

    Remove temporary info.php (PHP not available on static site plan)

--- info.php
+++
@@ -1,6 +0,0 @@
-<?php
-header('Content-Type: text/plain');
-echo "DOCUMENT_ROOT: " . ($_SERVER['DOCUMENT_ROOT'] ?? 'N/A') . "\n";
-echo "SCRIPT_FILENAME: " . ($_SERVER['SCRIPT_FILENAME'] ?? 'N/A') . "\n";
-echo "PWD: " . getcwd() . "\n";
-?>
